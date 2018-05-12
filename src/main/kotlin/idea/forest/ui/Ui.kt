cbe24a4412ec0db1f19ddc965ba47c641475187f
SerVB <servbul@yandex.ru>
2018-05-12 18:35:00 +0300

    Code cleanup

--- src/main/kotlin/idea/forest/ui/Ui.kt
+++ src/main/kotlin/idea/forest/ui/Ui.kt
@@ -4,23 +4,16 @@ import idea.forest.Forest
 import idea.forest.ForestRandoms
 import idea.forest.Updater
 import javafx.application.Application
-import javafx.event.ActionEvent
 import javafx.event.EventHandler
-import javafx.fxml.FXML
 import javafx.scene.control.Button
-import javafx.scene.control.Label
 import javafx.scene.control.TextArea
 import javafx.scene.control.TextField
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import tornadofx.*
 
-//val forest = Forest(3, 3, ForestRandoms())
-//
-
 class ForestView : View("Idea Forest") {
     override val root: BorderPane by fxml()
-//    val hello: Label by fxid()
 
     val reset: Button by fxid()
     val stepDelay: TextField by fxid()
@@ -33,7 +26,7 @@ class ForestView : View("Idea Forest") {
 
     init {
         updateText()
-//        reset.onAction = EventHandler { reset() }
+        reset.onAction = EventHandler { reset() }
         run.onAction = EventHandler { run() }
     }
 
