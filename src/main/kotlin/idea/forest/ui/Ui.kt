bb75b7e089e93db18bdb7eb0255620b1db96b5fa
SerVB <servbul@yandex.ru>
2018-05-12 18:30:13 +0300

    Add initial UI support

---
+++ src/main/kotlin/idea/forest/ui/Ui.kt
@@ -0,0 +1,68 @@
+package idea.forest.ui
+
+import idea.forest.Forest
+import idea.forest.ForestRandoms
+import idea.forest.Updater
+import javafx.application.Application
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
+import javafx.scene.layout.BorderPane
+import javafx.stage.Stage
+import tornadofx.*
+
+//val forest = Forest(3, 3, ForestRandoms())
+//
+
+class ForestView : View("Idea Forest") {
+    override val root: BorderPane by fxml()
+//    val hello: Label by fxid()
+
+    val reset: Button by fxid()
+    val stepDelay: TextField by fxid()
+    val stepCount: TextField by fxid()
+    val run: Button by fxid()
+
+    val logArea: TextArea by fxid()
+
+    var forest: Forest = Forest(3, 3, ForestRandoms())
+
+    init {
+        updateText()
+//        reset.onAction = EventHandler { reset() }
+        run.onAction = EventHandler { run() }
+    }
+
+    private fun updateText() {
+        logArea.text = "Лес создан\n"
+    }
+
+    private fun reset() {
+        forest = Forest(3, 3, ForestRandoms())
+        // TODO: Updater?
+    }
+
+    private fun run() {
+        val delay = stepDelay.text.toLong()
+        val n = stepCount.text.toInt()
+        for (_i in 1..n) {
+            Updater.start(n, delay)
+            logArea.appendText(ForestLogPrinter.getUpdates())
+        }
+    }
+}
+
+class ForestViewApp : App(ForestView::class) {
+    override fun start(stage: Stage) {
+        stage.isResizable = false
+        super.start(stage)
+    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(ForestViewApp::class.java, *args)
+}
